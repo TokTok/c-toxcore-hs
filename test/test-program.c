6ea67bec47535f384b6316df1002993f75c4b9f1
iphydf <iphydf@users.noreply.github.com>
2020-03-11 13:52:32 +0000

    Use bazel to build c-toxcore-hs.
    This works much better than our dlopen hack.

--- test/test-program.c
+++ test/test-program.c
@@ -1,80 +1,31 @@
-#define INCLUDE_TOXCORE 0
-
-#if INCLUDE_TOXCORE
-#include "../../c-toxcore/toxcore/crypto_core.h"
-#else
-#include "Network/Tox/CExport/CryptoCore_stub.h"
-#endif
-
-#include <dlfcn.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
-static void *must_dlsym(void *lib, char const *name)
-{
-  void *sym = dlsym(lib, name);
-  if (!sym) {
-    fprintf(stderr, "error resolving %s: %s\n", name, dlerror());
-    exit(EXIT_FAILURE);
-  }
-  return sym;
-}
+#include "crypto_core.h"
 
 int main(void)
 {
-  char path[1024] = { 0 };
-  if (!getcwd(path, sizeof path)) {
-    perror("getcwd");
-    return EXIT_FAILURE;
-  }
-  strncat(path, "/libtoxcore-hs.so", sizeof path - strlen(path) - 1);
+  printf("%u\n", random_u32());
+  printf("%lu\n", random_u64());
 
-  // Load library.
-  void *lib = dlopen(path, RTLD_NOW | RTLD_LOCAL);
-  if (!lib) {
-    fprintf(stderr, "error while loading libtoxcore-hs.so: %s\n", dlerror());
-    return EXIT_FAILURE;
-  }
-
-#define SYM(name) \
-  typeof(name) *dl_##name = must_dlsym(lib, #name)
-
-  // Resolve symbols.
-  SYM(decrypt_data_symmetric);
-  SYM(encrypt_data_symmetric);
-  SYM(encrypt_precompute);
-  SYM(random_u64);
-  SYM(random_u32);
-  SYM(random_nonce);
-
-  printf("%u\n", dl_random_u32());
-  printf("%lu\n", dl_random_u64());
-
-  uint8_t pk[32] = { 0, 1, 2, 3, 4 };
-  uint8_t sk[32] = { 0, 4, 3, 2, 1 };
+  const uint8_t pk[32] = { 0, 1, 2, 3, 4 };
+  const uint8_t sk[32] = { 0, 4, 3, 2, 1 };
   uint8_t ck[32] = { 0 };
-  dl_encrypt_precompute(pk, sk, ck);
+  encrypt_precompute(pk, sk, ck);
 
   uint8_t nonce[24] = { 0 };
-  dl_random_nonce(nonce);
+  random_nonce(nonce);
 
-  char plain[] = "hello world";
+  const char plain[] = "hello world";
   char encrypted[sizeof plain + 16] = { 0 };
-  dl_encrypt_data_symmetric(ck, nonce, (uint8_t *)plain, sizeof plain, (uint8_t *)encrypted);
+  encrypt_data_symmetric(ck, nonce, (const uint8_t *)plain, sizeof plain, (uint8_t *)encrypted);
 
   char plain2[sizeof plain] = { 0 };
-  dl_decrypt_data_symmetric(ck, nonce, (uint8_t *)encrypted, sizeof encrypted, (uint8_t *)plain2);
+  decrypt_data_symmetric(ck, nonce, (const uint8_t *)encrypted, sizeof encrypted, (uint8_t *)plain2);
 
   printf("%s\n", plain2);
-
-  // Unload library.
-  if (dlclose(lib) != 0) {
-    fprintf(stderr, "error while unloading libtoxcore-hs.so: %s\n", dlerror());
-    return EXIT_FAILURE;
-  }
 
   return strcmp(plain, plain2) == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
